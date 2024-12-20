c9ef2d9376dc84b2f446d06088563a5a6bc379fb
Mns2004 <manasareddymuddireddy@gmail.com>
2024-12-20 15:54:59 +0530

    Update settings.py

--- Weather/settings.py
+++ Weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=vuqm_s%h36)j9x66n1b(pnodin97b$ns=o83s4dk4dck=r7=j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.205.240.196','localhost',8000]
+ALLOWED_HOSTS = ['43.205.240.196','localhost','0.0.0.0']
 
 
 # Application definition
